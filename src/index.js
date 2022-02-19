43b603e709241e596be4f9637a31b1f78ebff877
Sahil  Joshi <sahiljoshi6378@gmail.com>
2022-02-19 15:07:15 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+ReactDOM.render(
+    <React.StrictMode>
+    <h1>Hello from todolingo</h1>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
