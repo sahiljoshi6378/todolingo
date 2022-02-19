248400de7723456dd819513362dc628d5bf569e5
Sahil  Joshi <sahiljoshi6378@gmail.com>
2022-02-19 20:38:36 +0530

    html and css for home page

--- src/components/TodoListContainor/TodoListContainor.jsx
+++ src/components/TodoListContainor/TodoListContainor.jsx
@@ -7,7 +7,10 @@ const TodoListContainor = () =>{
     return(
         <>  
         <div className="listcontainor__outer">
-   
+            <div className ="listcontainor__inner__display">
+
+            </div>
+       
         </div>
         </>
     )
@@ -18,3 +21,16 @@ const TodoListContainor = () =>{
 
 // exporting default TodoListContainor
 export default TodoListContainor
+
+
+
+// <div className="listcontainor__inner__stat__display">
+// <p className="stat_txt active__items__left__txt">5 items</p>
+// {/* <div className="filter__actions" >
+//     <p className="stat_txt action filter__action__all">All</p>
+//     <p className="stat_txt action filter__action__active">Active</p>
+//     <p className="stat_txt action filter__action__completed">Completed</p>
+// </div> */}
+// <p className="stat_txt action clear_all_action">Clear Completed</p>
+// <p className="stat_txt action clear_all_action">Clear Completed</p>
+// </div>
